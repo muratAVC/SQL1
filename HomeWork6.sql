6489512ee2c97bd3842203d0034cff81c4add136
muratAVC <110787002+muratAVC@users.noreply.github.com>
2022-10-29 09:15:47 +0300

    www.patika.dev

---
+++ HomeWork6.sql
@@ -0,0 +1,6 @@
+Select * from film
+Where replacement_cost Between 12.99 and 16.98;--16.99 dan küçük olma koşulu
+
+--film tablosunda bulunan tüm sütunlardaki verileri replacement 
+--cost değeri 12.99 dan büyük eşit ve 16.99 küçük 
+--olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
---
+++ HomeWork7.sql
@@ -0,0 +1,7 @@
+Select first_name, last_name From actor
+Where first_name In ('Penelope','Nick','Ed');
+
+
+--actor tablosunda bulunan first_name ve last_name sütunlardaki 
+--verileri first_name 'Penelope' veya 'Nick' veya 
+--'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)
