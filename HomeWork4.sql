bd1968ae76291d75236999b70b2172a640a8abad
muratAVC <110787002+muratAVC@users.noreply.github.com>
2022-10-29 08:46:21 +0300

    Add files via upload

---
+++ HomeWork4.sql
@@ -0,0 +1,3 @@
+Select * From customer
+Where first_name='Mary';
+-- last_name= 'Smith'
---
+++ HomeWork5.sql
@@ -0,0 +1,6 @@
+Select * From film
+Where (not length>50) and not(rental_rate=2.99 or rental_rate=4.99)  ;
+
+
+--film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda 
+--rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
